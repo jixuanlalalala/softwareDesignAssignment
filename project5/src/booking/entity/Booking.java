d6b84715cc9ccb0a46324d2cd834b0439f2c7b79
WillieBam <meilin.22@1utar.my>
2025-09-02 11:14:57 +0800

    added entity booking

--- project5/src/booking/entity/Booking.java
+++ project5/src/booking/entity/Booking.java
@@ -1,5 +1,63 @@
 package booking.entity;
 
+import java.time.LocalDate;
+
 public class Booking {
+
+private String bookingId;
+private String userId;
+private LocalDate startDate;
+private LocalDate endDate;
+private int updateCounter;
+
+
+public Booking(String bookingId, String userId, LocalDate startDate, LocalDate endDate, int updateCounter) {
+    this.bookingId = bookingId;
+    this.userId = userId;
+    this.startDate = startDate;
+    this.endDate = endDate;
+    this.updateCounter = updateCounter;
+}
+
+
+public String getBookingId() {
+    return bookingId;
+}
+
+
+public String getUserId() {
+    return userId;
+}
+
+
+public LocalDate getStartDate() {
+    return startDate;
+}
+
+
+public LocalDate getEndDate() {
+    return endDate;
+}
+
+
+public int getUpdateCounter() {
+    return updateCounter;
+}
+
+
+public void setStartDate(LocalDate startDate) {
+    this.startDate = startDate;
+}
+
+
+public void setEndDate(LocalDate endDate) {
+    this.endDate = endDate;
+}
+
+
+public void setUpdateCounter(int updateCounter) {
+    this.updateCounter = updateCounter;
+}
+
 
 }
